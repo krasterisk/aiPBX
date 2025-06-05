a1bf1df9c3c0b67f26a3e6a615be2037d541a2e8
krasterisk <info@krasterisk.ru>
2025-06-05 13:52:50 +0700

    Added in github actions for deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,7 @@ module.exports = {
     {
       name: 'aiPBX',
       script: './dist/main.js',
-      watch: false,
-      env: {
-        NODE_ENV: 'production'
-      },
-      env_production: {
-        NODE_ENV: 'production'
-      }
+      watch: false
     }
   ]
 }
