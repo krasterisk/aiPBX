d6c8c1c0eca1b7948471908a0343ef49707e484d
krasterisk <info@krasterisk.ru>
2025-10-02 18:10:25 +0700

    Fixed login loader

--- src/app/middleware/toastMiddleware.ts
+++ src/app/middleware/toastMiddleware.ts
@@ -1,12 +1,25 @@
-import { Middleware, isRejectedWithValue } from '@reduxjs/toolkit'
-import { toast } from 'react-toastify'
-import { getErrorMessage } from '@/shared/lib/functions/getErrorMessage'
+import { isRejectedWithValue, Middleware } from '@reduxjs/toolkit'
 
-export const toastMiddleware: Middleware = () => (next) => (action) => {
-  // если RTK Query вернул reject (ошибка)
+import { toast } from 'react-toastify'
+import i18n from '@/shared/config/i18n/i18n' // тот же самый экземпляр, что и в useTranslation
+
+export const toastMiddleware: Middleware = () => next => action => {
   if (isRejectedWithValue(action)) {
-    const errorMsg = getErrorMessage(action.payload)
-    toast.error(errorMsg)
+    const status = action?.payload?.status
+    const message =
+        action?.payload?.data?.message ||
+        action?.error?.message ||
+        'unknownError'
+
+    // Исключение для 404
+    if (status === 404 || status === 403) {
+      return next(action)
+    }
+
+    // Пробуем найти перевод
+    const translated = i18n.t(`${String(message)}`) || message
+
+    toast.error(translated)
   }
 
   return next(action)
