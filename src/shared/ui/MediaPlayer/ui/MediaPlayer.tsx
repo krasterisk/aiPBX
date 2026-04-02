842ee0e5bb980964bb2f83b41146ac159ba00d9f
krasterisk <info@krasterisk.ru>
2026-04-02 12:01:05 +0700

    Added player for http links [deploy:3]

--- src/shared/ui/MediaPlayer/ui/MediaPlayer.tsx
+++ src/shared/ui/MediaPlayer/ui/MediaPlayer.tsx
@@ -1,6 +1,7 @@
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
 import { classNames } from '@/shared/lib/classNames/classNames'
 import { HStack } from '../../redesigned/Stack'
+import { Headphones } from 'lucide-react'
 import cls from './MediaPlayer.module.scss'
 
 interface MediaPlayerProps {
@@ -14,12 +15,21 @@ export const MediaPlayer = memo((props: MediaPlayerProps) => {
     src
   } = props
 
+  const onPlay = useCallback(() => {
+    window.open(
+      src,
+      'audio_player',
+      'width=520,height=200,menubar=no,toolbar=no,location=no,status=no,resizable=yes'
+    )
+  }, [src])
+
   return (
     <HStack max className={classNames(cls.MediaPlayer, {}, [className])}>
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <audio controls src={src} preload="auto" className={cls.audio}>
-        Your browser does not support the audio element.
-      </audio>
+      <button className={cls.playButton} onClick={onPlay} type="button">
+        <Headphones size={18} className={cls.playButtonIcon} />
+        {/* eslint-disable-next-line i18next/no-literal-string */}
+        <span>Прослушать запись</span>
+      </button>
     </HStack>
   )
 })
